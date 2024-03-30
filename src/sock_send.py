407103cd89702a5c55e1cbea60e407ba12166386
Kshitij Aucharmal <kshitijaucharmal21@gmail.com>
2024-03-30 19:47:29 +0530

    Don't need threading anymore

--- src/sock_send.py
+++ src/sock_send.py
@@ -1,9 +1,8 @@
 import numpy as np
-import threading
 import socket
 
 HOST = "localhost"
-ports = range(65400, 65500)
+port = 65432
 
 msg = ""
 
@@ -13,25 +12,22 @@ def open_port(p):
     # start server
     s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
     s.bind((HOST, p))
+    print(f"{port} bound")
+
     s.listen()
     conn, addr = s.accept()
 
     print(f"Connected to {addr}")
 
     while True:
-        msg = f"Hello from {p}"
+        msg = f"Random number is: {np.random.random()}"
         conn.sendall(str(msg).encode())
-        cmd = conn.recv(1024)
-        if "exit" in cmd.decode():
+        cmd = conn.recv(1024).decode()
+        if "next" in cmd:
+            continue
+
+        if "exit" in cmd:
             break
 
 
-threads = []
-for p in ports:
-    thread = threading.Thread(target=open_port, args=(p,))
-    threads.append(thread)
-    thread.start()
-print("All ports bound", ports)
-
-for thread in threads:
-    thread.join()
+open_port(port)
